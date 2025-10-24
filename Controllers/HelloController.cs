7c2264fa215ab75402867a81da32c3238ee48e1f
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-10-24 23:19:52 +0530

    first web Api

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from .Net Core Web Api";
+        }
+    }
+}
---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemsController:ControllerBase
+    {
+        private static readonly List<Item> items = new List<Item>
+        {
+            new Item{Id=1,Name="Item 1"},
+            new Item{Id=2,Name="Item 2"},
+            new Item{Id=3,Name="Item 3"}
+        };
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return items;
+        }
+    }
+}
